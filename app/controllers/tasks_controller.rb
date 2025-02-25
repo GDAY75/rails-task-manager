f37afbda192fb41a55c2d62d1f153ad41c83bdbd
Gil DAYAN <gil.dayan@orange.fr>
2025-02-25 13:57:56 +0100

    tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    if params[:completed] == "1"
+      completed = true
+    else
+      completed = false
+    end
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    @task.update({completed: completed})
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
